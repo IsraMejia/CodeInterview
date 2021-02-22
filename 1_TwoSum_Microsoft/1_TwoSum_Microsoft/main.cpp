fd0d3f7c4f53824b16dc5c2ea35c5dc18f8021c6
IsraMejia <ihmaisrael@gmail.com>
2021-02-21 21:30:08 -0600

    TwoSum ready, with complexity of O(n^2)

---
+++ 1_TwoSum_Microsoft/1_TwoSum_Microsoft/main.cpp
@@ -0,0 +1,14 @@
+    //
+//  main.cpp
+//  1_TwoSum_Microsoft
+//
+//  Created by Israel Mejía on 21/02/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
